a16dfc36a0e86e344846733ffbd9e1f574f3e72d
DylanPrimera <dylanprimera98@gmail.com>
2020-05-31 18:56:11 -0500

    update slider

--- ageda-view/src/components/slider.js
+++ ageda-view/src/components/slider.js
@@ -2,37 +2,39 @@ import React from 'react'
 import uno from './img/sonrie.jpg'
 import dos from './img/estu2.jpg'
 import tre from './img/estu1.jpg'
-import { Carousel} from 'react-bootstrap';
 import './style/slider.css'
 class slider extends React.Component{
     render(){
         return <div>
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={uno}
-                    alt="First slide"
-                    id="slider"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={dos}
-                    alt="Second slide"
-                    id="slider"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={tre}
-                    alt="Third slide"
-                    id="slider"
-                    />
-                </Carousel.Item>
-            </Carousel>
+            <div id="slider" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                    <li data-target="#slider" data-slide-to="0" className="active"></li>
+                    <li data-target="#slider" data-slide-to="1"></li>
+                    <li data-target="#slider" data-slide-to="2"></li>
+                </ol>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img className="img-fluid" src={uno} />
+                        
+                    </div>
+                    <div className="carousel-item">
+                        <img className="img-fluid" src={dos} />
+        
+                    </div>
+                    <div className="carousel-item">
+                        <img className="img-fluid" src={tre} />
+        
+                    </div>
+                </div>
+                <a className="carousel-control-prev" href="#slider" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#slider" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
+            </div>
         </div>
     }
 }
--- ageda-view/src/index.js
+++ ageda-view/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Principal from './principal/principal'
+
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.min.js';
 ReactDOM.render(
   <Principal />,
   document.getElementById('root')
